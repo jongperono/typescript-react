db2d776b7f3a277e2decf1a9c406d33a54ead8c5
jong <jongperono@gmail.com>
2022-05-11 15:54:28 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,25 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo[]>([])
 
-  const handleAdd = () => {
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo("")
+      console.log('todo', todo)
+      console.log('set todo', setTodo)
+      console.log('todos', todos)
 
+    }
   }
 
   return (
     <div className="App">
       <span className="heading"> Tasktify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map(
+        todo => <li>{todo.todo}</li>
+      )}
     </div>
   )
 }
